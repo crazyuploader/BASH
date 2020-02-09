123875efd5bd50ff4b1ff37cd112c3ba96cb794e
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-02-09 14:14:57 +0530

    Update: git.sh
    Nothing big just add few more code ;p

--- git.sh
+++ git.sh
@@ -29,6 +29,18 @@ function Push(){
     git push origin master
 }
 
+function Remote_View(){
+    git remote -v
+}
+
+function Remote_Add(){
+    git remote add "$RNAME" "$RURL"
+}
+
+function Remote_Remove(){
+    git remote remove "$RNAME"
+}
+
 while true
 do
     clear
@@ -42,11 +54,15 @@ do
     echo "4. Git Commit"
     echo "5. Git Add & Commit"
     echo "6. Git Push"
-    echo "7. Exit"
+    echo "7. Git Remote View"
+    echo "8. Git Remote Add"
+    echo "9. Git Remote Remove"
+    echo "10. Exit"
     echo ""
     echo "Your Choice?"
     read -r choice
     if [[ $choice == 1 ]]; then
+        clear
         echo "Git Status"
         echo ""
         Status
@@ -54,18 +70,27 @@ do
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 2 ]]; then
+        clear
         echo "Git Update"
+        echo ""
+        echo "Note: - It always pull from 'origin/master'"
+        echo ""
         Update
         echo ""
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 3 ]]; then
+        clear
         echo "Git Add"
         echo ""
         echo "Usage -"
         echo ""
         echo "All Files --> '.' (period)"
         echo "Single File --> 'File_Name'"
+        echo ""
+        echo "Changes:"
+        echo ""
+        git status -s
         echo ""
         echo "Your Choice?"
         read -r Name
@@ -74,6 +99,7 @@ do
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 4 ]]; then
+        clear
         echo "Git Commit"
         echo ""
         echo "Enter Commit Message"
@@ -83,6 +109,7 @@ do
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 5 ]]; then
+        clear
         echo "Git Add & Commit"
         echo "Usage for git add -"
         echo ""
@@ -98,6 +125,7 @@ do
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 6 ]]; then
+        clear
         echo "Git Push"
         echo ""
         Push
@@ -105,7 +133,49 @@ do
         read -rp "Press enter to continue"
     fi
     if [[ $choice == 7 ]]; then
-        echo "Have a nice day!"
+        clear
+        echo "Git Remote View"
+        echo ""
+        Remote_View
+        echo ""
+        read -rp "Press enter to continue"
+    fi
+    if [[ $choice == 8 ]]; then
+        clear
+        echo "Git Remote Add"
+        echo ""
+        echo -e "Enter Remote Name"
+        read -r RNAME
+        echo ""
+        echo "Enter Remote URL for '${RNAME}'"
+        read -r RURL
+        Remote_Add
+        echo ""
+        echo "Remote '${RNAME}' of URL '${RURL}' added."
+        echo ""
+        read -rp "Press enter to continue"
+    fi
+    if [[ $choice == 9 ]]; then
+        clear
+        echo "Git Remote Remove"
+        echo ""
+        echo "Available Remotes"
+        echo ""
+        Remote_View
+        echo ""
+        echo "Enter Remote Name to be Removed"
+        read -r RNAME
+        echo ""
+        echo "Entered Remote to be Removed '${RNAME}'"
+        Remote_Remove
+        echo ""
+        echo "Remote '${RNAME}' Removed."
+        echo ""
+        read -rp "Press enter to continue"
+    fi
+    if [[ $choice == 10 ]]; then
+        clear
+        echo "Have a Nice Day!"
         echo "Exiting..."
         clear
         exit
