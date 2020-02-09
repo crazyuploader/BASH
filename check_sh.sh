d53b32d0541471a06f0ddab5af6d96e56e1ad44f
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-02-09 12:44:41 +0530

    Add & Update:
    check_sh.sh
    .travis.yml

---
+++ check_sh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Colors
+GREEN='\033[1;32m'
+NC='\033[0m'
+YELLOW='\033[1;33m'
+
+echo ""
+echo -e "${GREEN}" "Checking all thefiles with Shellcheck ${NC}"
+for f in *.sh; do echo ""; echo "Checking $f"; shellcheck "$f"; done
+echo ""
+echo -e "${YELLOW}" "Done! ${NC}"
