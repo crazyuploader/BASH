e040a64182add0d2a07564dff2c4f88b2a2372cf
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-02-12 22:30:20 +0530

    Add: update.sh

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+clear
+echo "Please wait, updating your repositories from GitHub"
+echo ""
+cd c && git fetch origin master && git pull origin master && cd ..
+echo ""
+cd collegestuff && git fetch origin master && git pull origin master && cd ..
+echo ""
+cd cpp && git fetch origin master && git pull origin master && cd ..
+echo ""
+cd python && git fetch origin master && git pull origin master && cd ..
+echo ""
+cd bash && git fetch origin master && git pull origin master && cd ..
+echo ""
+cd data && git fetch origin master && git pull origin master && cd ..
+echo "Done!"
+sleep 5
+clear
