1ccba33cbb36e15d372e8539ab772416047d7e67
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-02-12 22:37:10 +0530

    Update: check_sh.sh

--- check_sh.sh
+++ check_sh.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 YELLOW='\033[1;33m'
 
 echo ""
-echo -e "${GREEN}" "Checking all the files with Shellcheck ${NC}"
+echo -e "${GREEN}Checking all the files with Shellcheck${NC}"
 for f in *.sh; do echo ""; echo "Checking '$f'"; shellcheck "$f"; done
 echo ""
 echo -e "${YELLOW}Done!${NC}"
