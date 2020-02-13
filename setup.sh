01038602ad6fb6b1f97cc1e2f2f0e7f953afba22
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-02-13 20:23:46 +0530

    Update: setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ RED="\033[0;31m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 
+# Git Identity
+HUB_NAME="crazyuploader"
+HUB_EMAIL="49350241+crazyuploader@users.noreply.github.com"
+
 # Repositories
 BASH="https://github.com/crazyuploader/Bash.git"
 PYTHON="https://github.com/crazyuploader/Python.git"
@@ -132,26 +136,42 @@ curl -O https://raw.githubusercontent.com/crazyuploader/Bash/master/update.sh &&
 GIT_NAME=$(git config user.name)
 GIT_EMAIL=$(git config user.email)
 if [[ -z ${GIT_NAME} ]] && [[ -z ${GIT_EMAIL} ]]; then
-    echo "Your Git Credentials seems to be empty, wanna add?"
+    echo "Your Git Identity seems to be empty, wanna add?"
     NEWLINE
     echo "'y' for yes, anything else for no"
     echo "Choice?"
     read -r TEMP
     if [[ $TEMP = "y" ]]; then
-        echo "Enter Your Name: "
-        read -r G_NAME
-        git config --global user.name "${G_NAME}"
+        echo "If 'Jugal Kishore' press 'j', anything else if not"
         NEWLINE
-        echo -e "Git Name set to: ${GREEN}${G_NAME}${NC}"
-        clear
-        echo "Enter Your Email: "
-        read -r G_EMAIL
-        git config --global user.email "${G_EMAIL}"
-        NEWLINE
-        echo -e "Git Email set to: ${GREEN}${G_EMAIL}${NC}"
+        echo "Choice?"
+        read -r TEMP
+        if [[ $TEMP = "j" ]]; then
+            echo "Git Identity to be added:"
+            NEWLINE
+            echo -e "Username: ${GREEN}${HUB_NAME}${NC}"
+            echo -e "Email: ${GREEN}${HUB_EMAIL}${NC}"
+            NEWLINE
+            echo "Save?"
+            echo "Press 'y' for yes, anything else for no"
+            git config --global user.name "${HUB_NAME}"
+            git config --global user.email "${HUB_EMAIL}"
+        else
+            echo "Enter Your Name: "
+            read -r G_NAME
+            git config --global user.name "${G_NAME}"
+            NEWLINE
+            echo -e "Git Name set to: ${GREEN}${G_NAME}${NC}"
+            clear
+            echo "Enter Your Email: "
+            read -r G_EMAIL
+            git config --global user.email "${G_EMAIL}"
+            NEWLINE
+            echo -e "Git Email set to: ${GREEN}${G_EMAIL}${NC}"
+        fi
         clear
         git config --global credential.helper "cache --timeout=7200"
-        echo "${GREEN}Git Credentials Saved.${NC}"
+        echo "${GREEN}Git Identity Saved.${NC}"
     fi
 fi
 sleep 3
