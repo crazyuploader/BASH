4151f16d71a8c17c366ce089ff68e0fa106054ed
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-01 12:48:36 +0530

    Update: update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+# Created By Jugal Kishore -- 2020
+# Update Repositories Script
+
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+
 clear
-echo "Please wait, updating your repositories from GitHub"
+echo -e "${YELLOW}Please wait, updating your repositories from GitHub${NC}"
 echo ""
-cd c && git fetch origin master && git pull origin master && cd ..
-echo ""
-cd collegestuff && git fetch origin master && git pull origin master && cd ..
-echo ""
-cd cpp && git fetch origin master && git pull origin master && cd ..
-echo ""
-cd python && git fetch origin master && git pull origin master && cd ..
-echo ""
-cd bash && git fetch origin master && git pull origin master && cd ..
-echo ""
-cd data && git fetch origin master && git pull origin master && cd ..
-echo ""
+for f in */; do
+    if [[ ${f} != "and/" ]]; then
+        echo -e "Updating ${GREEN}${f}${NC}"
+        cd "$f" || { echo -e "Error while changing directory to ${RED}${f}${NC}"; exit 1; }
+        git pull
+        echo ""
+        cd ..
+    fi
+done
 echo "Done!"
-sleep 5
-clear
