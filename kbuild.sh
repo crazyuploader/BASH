de415cfc5fc8f3082ff8a33a00f5326a7e3aeaf7
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-02 10:13:51 +0530

    Update: kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -4,22 +4,22 @@
 # Kernel Automated Script
 
 function SET_ENVIRONMENT() {
-    mkdir clang
-    cd clang || exit
-    wget -nv https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r377782b.tar.gz
-    tar -xf clang-r377782b.tar.gz
-    rm clang-r377782b.tar.gz
+    cd "${TOOLCHAIN}" || { echo "Failure!"; exit 1; }
+    cd clang-r377782b || { echo "Failure!"; exit 1; }
     CLANG_DIR="$(pwd)"
     CC="${CLANG_DIR}/bin/clang"
     CLANG_VERSION="$(./bin/clang --version | grep 'clang version' | cut -c 37-)"
     cd ..
     git clone --depth=1 https://github.com/crazyuploader/AnyKernel3.git anykernel
-    git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc
-    git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc32
 }
 
-if [[ -z "${REPO_URL}" ]]; then
-    echo "'REPO_URL' variable not found, please set it first."
+if [[ -z ${TOOLCHAIN} ]]; then
+    echo "Don't know where to get compilers from"
+    exit 1
+fi
+
+if [[ -z "${KERNEL_REPO_URL}" ]]; then
+    echo "'KERNEL_REPO_URL' variable not found, please set it first."
     exit 1
 fi
 if [[ -z "${DEF_CONFIG}" ]]; then
@@ -27,31 +27,49 @@ if [[ -z "${DEF_CONFIG}" ]]; then
     exit 1
 fi
 
-git clone --depth=1 "${REPO_URL}" Kernel
-cd Kernel || exit
 SET_ENVIRONMENT
+
+echo ""
+git clone --depth=1 "${KERNEL_REPO_URL}" Kernel
+cd Kernel || exit
+
+# Variables
 PWD="$(pwd)"
 NAME="$(basename "${PWD}")"
 TIME="$(date +%d%m%y%H%M)"
 KERNEL_VERSION="$(make kernelversion)"
+
+# Exporting Few Stuff
 export ZIPNAME="${NAME}_Kernel-${TIME}.zip"
 export KERNEL_VERSION="${KERNEL_VERSION}"
-export ANYKERNEL_DIR="${PWD}/anykernel"
-export GCC_DIR="${PWD}/gcc/bin/aarch64-linux-android-"
-export GCC32_DIR="${PWD}/gcc32/bin/arm-linux-androideabi-"
+export ANYKERNEL_DIR="${TOOLCHAIN}/anykernel"
+export GCC_DIR="${TOOLCHAIN}/gcc/bin/aarch64-linux-android-"
+export GCC32_DIR="${TOOLCHAIN}/gcc32/bin/arm-linux-androideabi-"
+export KBUILD_BUILD_USER="crazyuploader"
+export KBUILD_BUILD_HOST="github.com"
 export CC="${CC}"
 export KBUILD_COMPILER_STRING="${CLANG_VERSION}"
 export ARCH="arm64"
 export SUBARCH="arm64"
+
+# Telegram Message
+echo ""
 curl -s -X POST https://api.telegram.org/bot"${BOT_API_TOKEN}"/sendMessage -d text="CI Build -- ${NAME} at Version: ${KERNEL_VERSION}" -d chat_id="${KERNEL_CHAT_ID}" -d parse_mode=HTML
 START="$(date +"%s")"
 echo ""
-echo "Compiling ${NAME} at version: ${KERNEL_VERSION}"
+echo "Compiling ${NAME} at version: ${KERNEL_VERSION} with Clang Version: ${CLANG_VERSION}"
+
+# Compilation
 echo ""
 make O=out ARCH=arm64 "${DEF_CONFIG}"
 make -j"$(nproc --all)" O=out ARCH=arm64 CC="${CC}" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="${GCC_DIR}" CROSS_COMPILE_ARM32="${GCC32_DIR}"
+
+# Time Difference
 END="$(date +"%s")"
 DIFF="$((END - START))"
+
+# Zipping
+echo ""
 if [ -f "$(pwd)/out/arch/arm64/boot/Image.gz-dtb" ]
 	then
   	cp "$(pwd)/out/arch/arm64/boot/Image.gz-dtb" "${ANYKERNEL_DIR}"
