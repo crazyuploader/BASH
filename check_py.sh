d7e5d62e6c5250f5541d0908019ee02ebbc61cf7
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-24 18:44:49 +0530

    Update: check_py.sh

--- check_py.sh
+++ check_py.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Colors
-GREEN='\033[1;32m'
-NC='\033[0m'
-YELLOW='\033[1;33m'
+GREEN="\033[1;32m"
+NC="\033[0m"
+YELLOW="\033[1;33m"
 
 echo -e "${YELLOW}Python Linter${NC}"
 echo ""
-echo -e "${GREEN}Checking all the Python files with Pyflakes ${NC}"
+echo -e "${GREEN}Checking all the files with Pyflakes ${NC}"
 for f in *.py; do echo ""; echo "Checking '$f'"; pyflakes "$f"; done
 echo ""
 echo -e "${YELLOW}Pyflakes Done!${NC}"
 echo ""
 echo -e "${GREEN}Checking all the files with Flake8 ${NC}"
-for f in *.py; do echo ""; echo "Checking '$f'"; echo "$(flake8 "${f}" --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
+for f in *.py; do echo ""; echo "Checking '$f'"; echo "$(flake8 "${f}" --count --ignore=C901 --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
 echo ""
 echo -e "${YELLOW}Flake8 Done!${NC}"
 FILES=0
