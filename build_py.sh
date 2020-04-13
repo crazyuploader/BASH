2d1bca63ecb89762145df62778763e1c24c51790
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-13 22:15:19 +0530

    Update
    Update:
    build_cpp.sh
    build_py.sh
    check_py.sh

--- build_py.sh
+++ build_py.sh
@@ -10,45 +10,61 @@ RED="\033[0;31m"
 # Main Script
 clear
 echo -e "${GREEN}" "Available Files -${NC}"
-for f in *.py; do echo "$f"; done
+for f in *.py; do echo "${f}"; done
 echo ""
-echo -e "Enter File Name -- ${YELLOW}Without Extension${NC}"
+echo -e "${RED}Type 'quit' to exit${NC}"
+echo ""
+echo -e "Enter File Name -- ${YELLOW}Without '.py' Extension${NC}"
 read -r fname
+if [[ ${fname} == "quit" ]]; then
+    clear
+    echo "Choosing to quit? Off you go!"
+    echo ""
+    echo -e "${MAGENTA}K Thanks, Bye!${NC}"
+    sleep 2
+    exit
+fi
 if [[ -f ${fname}.py ]]; then
     clear
-    echo "Running $fname.py with Python3"
+    echo "Running '${fname}.py' with Python3"
     sleep 2
     clear
-    python3 "$fname.py"
+    python3 "${fname}.py"
 else
-    echo -e "${RED}" "File Does not Exist!${NC}"
+    clear
+    echo -e "'${fname}' ${RED}does not exist!${NC}"
+    echo "Check the file name, and try again"
+    echo ""
     echo "Exiting!!"
-    echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
-    sleep 1
+    echo ""
+    echo -e "${MAGENTA}K Thanks, Bye!${NC}"
+    sleep 2
     clear
     exit
 fi
 echo ""
-echo -e "${YELLOW}" "Show Source Code?${NC}"
+echo -e "${YELLOW}Show Source Code?${NC}"
 echo ""
 echo "'y' for yes and anything else for no"
 read -r temp
-if [[ $temp = "y" ]]; then
+if [[ ${temp} = "y" ]]; then
     clear
+    echo -e "${YELLOW}///// Source Code for '${fname}.py' /////${NC}"
+    echo ""
     cat "${fname}.py"
     sleep 5
     echo ""
-    echo "Press 'y' to Exit"
+    echo -e "Press 'y' to ${RED}Exit${NC}"
     read -r temp
-    if [[ $temp = "y" ]]; then
+    if [[ ${temp} = "y" ]]; then
         clear
-        echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
-        sleep 3
+        echo -e "${MAGENTA}K Thanks, Bye!${NC}"
+        sleep 2
         exit
     fi
 else
     clear
-    echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
+    echo -e "${MAGENTA}K Thanks, Bye!${NC}"
     sleep 2
     exit
 fi
