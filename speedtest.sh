48ee1856a07d57284e758d9ba514558bb7cb682b
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-10-16 17:40:40 +0530

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Script to install official Speedtest.net CLI
+# NOTE: Only bionic, xeniel are supported as of now
+
+sudo apt-get install gnupg1 apt-transport-https dirmngr
+export INSTALL_KEY=379CE192D401AB61
+export DEB_DISTRO=$(lsb_release -sc)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+sudo apt-get install speedtest
