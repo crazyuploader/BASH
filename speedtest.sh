57fc735f403f16b4fa0ab59feba71fbb3de26bd5
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-10-20 23:32:13 +0530

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Script to install official Speedtest.net CLI
+# Script to install official Speedtest.net CLI on Ubuntu
 # NOTE: Only bionic, xeniel are supported as of now
 
 # Colors
@@ -32,7 +32,7 @@ DEB_DISTRO="$(lsb_release -sc)"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${INSTALL_KEY}
 echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee  /etc/apt/sources.list.d/speedtest.list
 apt-get update
-apt-get install -y speedtest
+apt-get install -y speedtest 1> /dev/null
 echo ""
 SPEEDTEST="$(command -v speedtest)"
 if [[ -n "${SPEEDTEST}" ]]; then
