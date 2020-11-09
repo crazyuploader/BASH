710221f008a87c13dc197fa9fd83253112a1ac2e
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-11-09 06:56:20 +0530

    Update setup.sh

--- termux/setup.sh
+++ termux/setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Setting your Termux App"
+clear
+echo "Setting up Termux App"
 echo ""
 apt-get update
 apt-get upgrade -y
 apt-get install -y openssh dnsutils nano \
-                   git python nodejs-lts yarn
+                   git python nodejs-lts yarn \
+                   mtr traceroute
 termux-setup-storage
 echo "Done!"
