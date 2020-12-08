18e995a72eaac9e344cf2631aea9e6232307670a
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-12-08 14:59:31 +0530

    Create install.sh

---
+++ atlas-ripe/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+# Script to install RIPE Atlas Software Probe
+
+# Install Required Packages
+apt-get update
+apt-get -y install \
+        git 
+        tar
+        fakeroot
+        libssl-dev
+        libcap2-bin
+        autoconf
+        automake
+        libtool
+        build-essential
+
+BRANCH="${1}"
+
+if [[ -z "${BRANCH}" ]]; then
+    git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git
+else
+    git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git -b "${BRANCH}"
+fi
+
+./ripe-atlas-software-probe/build-config/debian/bin/make-deb
+sudo dpkg -i atlasswprobe*.deb
+echo ""
+echo -e "Public Key -\n\t$(cat /var/atlas-probe/etc/probe_key.pub)"
+echo ""
