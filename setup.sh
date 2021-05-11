5a2388915e5b29bb4734e7c9985e2237bb1cfcbb
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-05-11 12:57:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ HUB_NAME="crazyuploader"
 HUB_EMAIL="49350241+crazyuploader@users.noreply.github.com"
 
 # Repositories
-BASH="git@github.com:crazyuploader/Bash.git"
+BASH="git@github.com:crazyuploader/BASH.git"
 PYTHON="git@github.com:crazyuploader/Python.git"
 CPP="git@github.com:crazyuploader/CPP.git"
 C="git@github.com:crazyuploader/C.git"
