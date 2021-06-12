f26f76c14a98dd7e7bcb3ea8503cee2f4450bf28
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-06-12 11:28:52 +0000

    Rewrite setup.sh Script

--- setup.sh
+++ setup.sh
@@ -1,261 +1,62 @@
 #!/usr/bin/env bash
-
-# Created By Jugal Kishore -- 2021
-# Setup Helper v2.0
+set -eu
+set -o pipefail
 
 # Colors
-NC="\033[0m"
-RED="\033[0;31m"
 GREEN="\033[1;32m"
+NC="\033[0m"
 YELLOW="\033[1;33m"
+RED="\033[0;31m"
 
-# Git Identity
-HUB_NAME="crazyuploader"
-HUB_EMAIL="49350241+crazyuploader@users.noreply.github.com"
-
-# Repositories
-BASH="git@github.com:crazyuploader/BASH.git"
-PYTHON="git@github.com:crazyuploader/Python.git"
-TIL="git@github.com:crazyuploader/TIL.git"
-PINGER="git@github.com:crazyuploader/Pinger.git"
-OSL="git@github.com:crazyuploader/OSL.git"
-DOCKER="git@github.com:crazyuploader/Docker-Builder.git"
-
-# Custom Functions
-# 'CD' changes the directory or throws an error, and exists.
-function CD() {
-	cd "${1}" || {
-		echo -e "${RED}Failure${NC}"
-		exit 1
-	}
-}
-
-# 'NEWLINE' prints a new line.
-function NEWLINE() {
-	echo ""
-}
-
-# 'MKD' makes a directory, and enters it.
-function MKD() {
-	if [[ ! -d "${1}" ]]; then
-		mkdir "$1"
-		CD "$1"
-		echo -e "${GREEN}'${1}' directory created.${NC}"
-	else
-		echo -e "${YELLOW}'${1}' already exists, nevermind.${NC}"
-		CD "$1"
-	fi
-}
-
-# 'CLONE' checks if the directory exists, if it doesn't git clone or else enters the directory git pull.
-function CLONE() {
-	if [[ ! -d "${2}" ]]; then
-		git clone "${1}" "${2}"
-		NEWLINE
-		echo "Done!"
-	else
-		echo -e "${YELLOW}'${2}' directory exists, pulling the latest changes instead.${NC}"
-		CD "${2}"
-		NEWLINE
-		echo -e "${YELLOW}By Default Script will only pull from remote 'origin' & default branch${NC}"
-		NEWLINE
-		git pull
-		NEWLINE
-		echo "Done!"
-		CD ..
-	fi
-}
-
-function KSETUP() {
-	clear
-	if [[ "${1}" == "CLI" || "${1}" == "cli" ]]; then
-		return 0
-	fi
-	echo -e "${GREEN}Kernel Environment Setup${NC}"
-	NEWLINE
-	echo "Install Kernel Building Dependencies?"
-	echo "'y' for yes, and anything for no"
-	read -r TEMP
-	if [[ "${TEMP}" = "y" ]]; then
-		sudo apt-get update
-		NEWLINE
-		sudo apt-get upgrade -y
-		NEWLINE
-		sudo apt-get install -y \
-			curl \
-			git \
-			python3 \
-			python3-pip \
-			bc \
-			tar \
-			make \
-			wget \
-			gcc \
-			python-is-python3 \
-			clang \
-			libssl-dev \
-			zip \
-			mplayer \
-			shellcheck
-
-	else
-		echo "Installing nothing, off you go!"
-		clear
-	fi
-}
-
-function FUNCTIONS() {
-	clear
-	echo -e "${GREEN}Setting Bash Functions${NC}"
-	NEWLINE
-	if [[ -f "${HOME}/.bashrc" ]]; then
-		echo "'.bashrc' exists"
-		sed -i '/#<>/,$d' ~/.bashrc
-	else
-		echo "'.bashrc' doesn't exist, creating it"
-		echo "# Script file executed whenever a user logs into bash" >~/.bashrc
-	fi
-	echo -e "\n" >>~/.bashrc
-	curl -s https://raw.githubusercontent.com/crazyuploader/Misc/master/functions >>~/.bashrc
-	NEWLINE
-	echo -e "${YELLOW}'bashrc' file updated!${NC}"
-}
-
-START=$(date +"%s") # Start Time Reference
-clear
-if [[ "${1}" == "CLI" || "${1}" == "cli" ]]; then
-	echo -e "${YELLOW}Running in CLI Mode\nWould skip kernel setup${NC}"
-	echo ""
-fi
-GIT="$(command -v git)"
-if [[ -z ${GIT} ]]; then # Checking if git is installed or not, if it is not, ask to install or not.
-	echo -e "${YELLOW}'git' not installed.${NC}"
-	NEWLINE
-	echo "Install?"
-	echo "Press 'y' to yes and anything else to exit."
-	read -r temp
-	if [[ ${temp} = "y" ]]; then
-		NEWLINE
-		echo "Installing 'git'"
-		NEWLINE
-		sudo apt-get update
-		NEWLINE
-		sudo apt-get install -y git
-	else
-		NEWLINE
-		echo -e "${RED}Script can't work without 'git'${NC} :("
-		echo "Exiting..."
-		sleep 3
-		clear
-		exit
-	fi
+# Root Check
+if [[ "${EUID}" -ne "0" ]]; then
+	echo -e "${YELLOW}Run this script as root!${NC}"
+	echo -e "${RED}Exiting...${NC}"
+	exit 1
 else
-	echo -e "${GREEN}'git' installed, good to go.${NC}"
+	echo -e "${GREEN}Script running as root, continuing...${NC}"
+	sleep 1
 fi
-sleep 1
-clear
-echo "Setup Main Script"
-NEWLINE
-echo -e "Your OS:" "${GREEN}" "$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')${NC}"
-echo -e "Your Bash: ${GREEN}${BASH_VERSION}${NC}"
-sleep 3
-CD ~
-clear
-echo "Making Working Directory"
-NEWLINE
-MKD Desktop
-NEWLINE
-MKD work
-NEWLINE
-echo "Done!"
-NEWLINE
-echo -e "${YELLOW}Getting GitHub Repositories${NC}"
-NEWLINE
-sleep 1
-clear
-echo -e "Python at:${GREEN} ${PYTHON}${NC}"
-NEWLINE
-CLONE "$PYTHON" python
-sleep 1
-clear
-echo -e "Bash at:${GREEN} ${BASH}${NC}"
-NEWLINE
-CLONE "$BASH" bash
-sleep 1
-clear
-echo -e "Docker at: ${GREEN} ${DOCKER}${NC}"
-NEWLINE
-CLONE "$DOCKER" docker
-clear
-echo -e "TIL at: ${GREEN} ${TIL}${NC}"
-NEWLINE
-sleep 1
-clear
-echo -e "Pinger at: ${GREEN} ${PINGER}${NC}"
-NEWLINE
-sleep 1
-clear
-echo -e "OSL at: ${GREEN} ${OSL}${NC}"
-NEWLINE
-sleep 1
-clear
-echo "Getting Update Helper in current directory"
-curl -O https://raw.githubusercontent.com/crazyuploader/Bash/master/update.sh && chmod +x update.sh
-clear
-GIT_NAME=$(git config user.name)
-GIT_EMAIL=$(git config user.email)
-if [[ -z ${GIT_NAME} ]] && [[ -z ${GIT_EMAIL} ]]; then
-	echo "Your Git Identity seems to be empty, wanna add?"
-	NEWLINE
-	echo "'y' for yes, anything else for no"
-	echo "Choice?"
-	read -r TEMP
-	if [[ $TEMP = "y" ]]; then
-		echo "If 'Jugal Kishore' press 'j', anything else if not"
-		NEWLINE
-		echo "Choice?"
-		read -r TEMP
-		if [[ $TEMP = "j" ]]; then
-			echo "Git Identity to be added:"
-			NEWLINE
-			echo -e "Username: ${GREEN}${HUB_NAME}${NC}"
-			echo -e "Email: ${GREEN}${HUB_EMAIL}${NC}"
-			NEWLINE
-			echo "Save?"
-			echo "Press 'y' for yes, anything else for no"
-			git config --global user.name "${HUB_NAME}"
-			git config --global user.email "${HUB_EMAIL}"
-		else
-			echo "Enter Your Name: "
-			read -r G_NAME
-			git config --global user.name "${G_NAME}"
-			NEWLINE
-			echo -e "Git Name set to: ${GREEN}${G_NAME}${NC}"
-			clear
-			echo "Enter Your Email: "
-			read -r G_EMAIL
-			git config --global user.email "${G_EMAIL}"
-			NEWLINE
-			echo -e "Git Email set to: ${GREEN}${G_EMAIL}${NC}"
-		fi
-		clear
-		git config --global credential.helper "cache --timeout=7200"
-		echo -e "${GREEN}Git Identity Saved.${NC}"
-	fi
-fi
-echo -e "Git Identity -"
-NEWLINE
-echo -e "${GREEN}Saved Git Email:${NC} $(git config user.email)"
-NEWLINE
-echo -e "${GREEN}Saved Git Name:${NC} $(git config user.name)"
-NEWLINE
-sleep 2
-KSETUP "${1}"
-FUNCTIONS
-sleep 2
-NEWLINE
-END=$(date +"%s")     #Stop Time Reference
-DIFF=$((END - START)) # Difference between 'start' and 'stop' time Reference
-echo -e "Script ended in: ${YELLOW}$((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s).${NC}"
-NEWLINE
-echo -e "Created by: ${GREEN}Jugal Kishore${NC}"
+
+# Installing Package(s)
+apt-get update
+apt-get install -y --no-install-recommends \
+                    git \
+                    curl \
+                    python3 \
+                    python3-pip \
+                    wget \
+                    nano \
+                    tar \
+                    zip \
+                    unzip \
+                    python-is-python3 \
+                    neofetch \
+                    gpg \
+                    ssh \
+                    xz-utils \
+                    iputils-ping \
+                    traceroute \
+                    mtr-tiny \
+                    net-tools \
+                    nmap \
+                    iftop \
+                    tmux \
+                    screen \
+                    gpg-agent \
+                    jq \
+                    python3-venv \
+                    tree \
+                    shellcheck 1> /dev/null
+
+# Installing Docker and Docker Compose
+curl -sL https://get.docker.com | bash -
+
+# Installing Speedtest CLI
+curl -sL https://raw.githubusercontent.com/crazyuploader/BASH/master/install/speedtest.sh | bash -
+
+# Installing Brave Browser
+curl -sL https://raw.githubusercontent.com/crazyuploader/BASH/master/install/brave-browser.sh | bash -
+
+# Installing ffsend
+curl -sL https://raw.githubusercontent.com/crazyuploader/BASH/master/install/ffsend.sh | bash -
