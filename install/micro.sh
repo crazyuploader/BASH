64897f3b259664a79f27f5abb2d0b543cf596936
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2021-06-13 12:38:05 +0530

    Add install/micro.sh

---
+++ install/micro.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+# Install Script for Micro Text Editior
+# https://github.com/zyedidia/micro
+
+function move() {
+    if [[ -z "$(command -v sudo)" ]]; then
+        mv ./micro /usr/bin
+    else
+        sudo mv ./micro /usr/bin
+    fi
+}
+
+curl https://getmic.ro | bash
+move
+
+echo "Micro Installed!"
+micro --version
