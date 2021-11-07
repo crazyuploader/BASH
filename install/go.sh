f474df27ce3560e3f4bb9c05be1cd14e10a0dd08
Jugal Kishore <me@devjugal.com>
2021-11-07 14:19:27 +0530

    Update go.sh

--- install/go.sh
+++ install/go.sh
@@ -48,7 +48,8 @@ echo -e "${GREEN}Download completed...${NC}"
 echo ""
 if [[ -f "/home/jungle/.bashrc" ]]; then
 	echo "PATH=\"${PATH}:/usr/local/go/bin\"" >>/home/jungle/.bashrc
-	echo "export PATH" >>/home/jungle/.bashrc
+	echo "export GOROOT=\"/usr/local/go"\" >> /home/jungle/.bashrc
+	echo "export GOPATH==\"/home/jungle/go"\" >> /home/jungle/.bashrc
 	echo ""
 	echo -e "${GREEN}Go Lang $(go version) is now installed.${NC}"
 else
